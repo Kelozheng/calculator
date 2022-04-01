29a1e99c6bffc63ffb791020f8f7a7408ed5b050
Kelozheng <100518138+Kelozheng@users.noreply.github.com>
2022-04-01 18:15:57 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace mycalculator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose the calculator (+,-,*,/):");
+            string a = Console.ReadLine();
+            Console.WriteLine("Please enter the first number:");
+            float b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the second number:");
+            float c = float.Parse(Console.ReadLine());
+            if (a == "+")
+            {
+                Console.WriteLine("the result is ");
+                Console.WriteLine(b + c);
+            }
+
+            else if (a == "-")
+            {
+                Console.WriteLine("the result is ");
+                Console.WriteLine(b - c);
+            }
+
+            else if (a == "*" )
+            {
+                Console.WriteLine("the result is ");
+                Console.WriteLine(b * c);
+            }
+            else if (a == "/")
+            {
+                Console.WriteLine("the result is ");
+                Console.WriteLine(b / c);
+            }
+            else
+                Console.WriteLine("Invalid calculator, Please try again!");
+            Console.ReadLine();
+
+        }
+    }
+}
